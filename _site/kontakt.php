197a9d7005060e9b0963573afd52f76ec5f4f6e4
malejpstros <michal.kurz.frajer@gmail.com>
2015-07-06 12:12:02 +0000

    First steps in the app

---
+++ _site/kontakt.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width initial-scale=1" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>Your awesome title</title>
+    <meta name="description" content="Write an awesome description for your new site here. You can edit this line in _config.yml. It will appear in your document head meta (for Google search results) and in your feed.xml site description.
+">
+    
+    <link rel="stylesheet" href="./css/reset.css">
+
+    <link rel="stylesheet" href="./css/main.css">
+    <link rel="canonical" href="http://yourdomain.com/kontakt.php">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+</head>
+
+  <body>
+    <nav class="navbar navbar-default">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" style="font-weight:bold" href="index.html">ProFond</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav nav-custom navbar-blockafterlogo">
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+      
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+    
+    
+        <?php 
+    echo "ahoj";
+?>
+    
+    
+    
+    
+    
+
+    <footer class="site-footer">
+  <div class="container">
+    
+    <div class="row" style="position:relative;">
+      
+      
+      <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 footersegment" style="position:relative;">
+            <div class="fullwidthbgcolor"></div>
+            <div class="inlineblock" style="width:49%; color: white;">
+              <ul>
+                <li><a href="kontakt.html" class="link" style="color:white"><b>Kontaktujte nás</b></a></li>
+                <li><i class="fa fa-phone"></i>739 154 548</li>
+                <li><i class="fa fa-envelope-o"></i><a href="mailto:info@profond.cz" class="link">info@profond.cz</a></li>
+                <li><i class="fa fa-facebook"></i><a href="http://nasfacebook.cz" class="link" style="color:white;">ProFondCZ</a></li>
+                <li><i class="fa fa-twitter"></i><a href="http://nasfacebook.cz" class="link" style="color:white;">@ProFondCZ</a></li>
+              </ul>
+            </div>
+         
+            <div class="inlineblock" style="width: 49%; color: white;">
+              <div class="napistenam">
+                  <a href="kontakt.html" class="link">
+                    <i class="fa fa-file-text-o" ></i>Napište nám
+                  </a>
+              </div>
+              <div >
+                <ul>
+                  <li><i class="fa fa-home"></i>Kancelář:</li>
+                  <li><i></i>Radlická 751/139e</li>
+                  <li><i></i>Praha 5 - 155 00</li>
+                </ul>
+              </div>
+            </div>
+            
+      </div>
+      
+      <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 footersegment" style="position:relative;">
+          <div class="fullwidthbgcolor firstextrablock"></div>
+
+          <div class="inlineblock" style="width:49%; color: white;">
+              <ul>
+                <li><a href="kontakt.html" class="link" style="color:white"><b>Kontaktujte nás</b></a></li>
+                <li><i class="fa fa-phone"></i>739 154 548</li>
+                <li><i class="fa fa-envelope-o"></i><a href="mailto:info@profond.cz" class="link">info@profond.cz</a></li>
+                <li><i class="fa fa-facebook"></i><a href="http://nasfacebook.cz" class="link" style="color:white;">ProFondCZ</a></li>
+                <li><i class="fa fa-twitter"></i><a href="http://nasfacebook.cz" class="link" style="color:white;">@ProFondCZ</a></li>
+              </ul>
+            </div>
+         
+            <div class="inlineblock" style="width: 49%; color: white;">
+              <div class="napistenam" >
+                  <a href="kontakt.html" class="link">
+                    <i class="fa fa-file-text-o" ></i>Napište nám
+                  </a>
+              </div>
+              <div >
+                <ul>
+                  <li><i class="fa fa-home"></i>Kancelář:</li>
+                  <li><i></i>Radlická 751/139e</li>
+                  <li><i></i>Praha 5 - 155 00</li>
+                </ul>
+              </div>
+            </div>
+            
+      </div>
+          
+
+      <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 footersegment thetextaligningproperty" style="position:relative;">
+        
+        <div class="fullwidthbgcolor secondextrablock"></div>
+
+        <div class="inlineblock" style="width:49%; color: white;">
+            <ul>
+              <li><a href="kontakt.html" class="link" style="color:white"><b>Kontaktujte nás</b></a></li>
+              <li><i class="fa fa-phone"></i>739 154 548</li>
+              <li><i class="fa fa-envelope-o"></i><a href="mailto:info@profond.cz" class="link">info@profond.cz</a></li>
+              <li><i class="fa fa-facebook"></i><a href="http://nasfacebook.cz" class="link" style="color:white;">ProFondCZ</a></li>
+              <li><i class="fa fa-twitter"></i><a href="http://nasfacebook.cz" class="link" style="color:white;">@ProFondCZ</a></li>
+            </ul>
+          </div>
+       
+          <div class="inlineblock" style="width: 49%; color: white;">
+            <div class="napistenam" >
+                <a href="kontakt.html" class="link">
+                  <i class="fa fa-file-text-o" ></i>Napište nám
+                </a>
+            </div>
+            <div >
+              <ul>
+                <li><i class="fa fa-home"></i>Kancelář:</li>
+                <li><i></i>Radlická 751/139e</li>
+                <li><i></i>Praha 5 - 155 00</li>
+              </ul>
+            </div>
+      </div>
+      
+      </div>
+    </div>
+    
+    
+    
+  </div>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+</footer>
+
+  </body>
+</html>
